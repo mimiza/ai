7883748dbf304b29797bf989bdee61b715f56d55
MIMIZA <dev@mimiza.com>
2022-06-05 10:30:22 +0700

    update

--- test.js
+++ test.js
@@ -1,18 +1,45 @@
 import Network from "./Network.js"
 
-const data = [
-    { input: [0, 1], output: [1] },
-    { input: [1, 0], output: [1] },
-    { input: [0, 0], output: [0] },
-    { input: [1, 1], output: [0] }
+const XOR = [
+    {input: [0, 1], output: [1]},
+    {input: [1, 0], output: [1]},
+    {input: [0, 0], output: [0]},
+    {input: [1, 1], output: [0]}
 ]
 
-const network = new Network({ layers: [2, 10, 10, 1] })
+const AND = [
+    {input: [0, 1], output: [0]},
+    {input: [1, 0], output: [0]},
+    {input: [0, 0], output: [0]},
+    {input: [1, 1], output: [1]}
+]
 
-for (let i = 0; i < 20000; i++) data.forEach(d => network.train(d.input, d.output))
+const OR = [
+    {input: [0, 1], output: [1]},
+    {input: [1, 0], output: [1]},
+    {input: [0, 0], output: [0]},
+    {input: [1, 1], output: [1]}
+]
 
-for (let i = 0; i < 50; i++)
-    data.forEach(d => {
-        const test = network.calculate(d.input)
-        console.log({ input: d.input, output: Math.round(test[0]), check: Math.round(test[0]) === d.output[0] })
+const RAND = [
+    {input: [0, 1], output: [0]},
+    {input: [1, 0], output: [1]},
+    {input: [0, 0], output: [1]},
+    {input: [1, 1], output: [0]}
+]
+
+const test = (data) => {
+    const results = []
+    const network = new Network({layers:[2,10,10,1]})
+    for (let i = 0; i < 4000; i++) data.forEach(d => network.train(d.input, d.output))
+    for (let i = 0; i < 10; i++) data.forEach(d => {
+        const r = network.calculate(d.input)
+        results.push({input: d.input, output: Math.round(r[0]), accurate: Math.round(r[0]) === d.output[0]})
     })
+    return results
+}
+
+console.log("XOR", test(XOR))
+console.log("AND", test(AND))
+console.log("OR", test(OR))
+console.log("RAND", test(RAND))
