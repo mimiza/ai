0643d93b6733a4429a59a11e8dab785f0ebbcd30
MIMIZA <dev@mimiza.com>
2022-07-12 18:56:43 +0700

    making progress in Visualization

--- Visualization/Visualization.js
+++ Visualization/Visualization.js
@@ -1,6 +1,13 @@
 import Network from "../Network.js"
 
-const network = new Network({ layers: [2, 4, 4, 2] }).encode()
+const XOR = [
+    { input: [0, 1], output: [1] },
+    { input: [1, 0], output: [1] },
+    { input: [0, 0], output: [0] },
+    { input: [1, 1], output: [0] }
+]
+
+const network = new Network({ layers: [2, 0, 1] })
 
 const xmlns = "http://www.w3.org/2000/svg"
 
@@ -15,40 +22,41 @@ const draw = (name, attributes) => {
 const present = (data = {}) => {
     const w = svg.width.baseVal.value
     const h = svg.height.baseVal.value
+    const lw = w / data.l.length // Layer width.
     const map = {}
     data.l.forEach((layer, lindex) => {
         const neurons = layer.n || layer
         neurons.forEach((neuron, nindex) => {
-            const lw = w / data.l.length // Layer width.
             const rh = h / neurons.length // Row height.
             const r = lw * 0.2 // Neuron radius.
             const x = lw * lindex + lw / 2
             const y = rh * nindex + rh / 2
-            draw("circle", {
-                cx: x,
-                cy: y,
-                r,
-                "fill-opacity": 0,
-                stroke: "red",
-                "stroke-width": 2
-            })
-            map[neuron["#"]] = { x, y, r }
+            draw("circle", { cx: x, cy: y, r, fill: "none", stroke: "red", "stroke-width": 2 })
+            map[neuron] = { x, y, r }
         })
     })
     data.c.forEach(connection => {
-        const x1 = map[connection.from].x + map[connection.from].r
-        const y1 = map[connection.from].y
-        const x2 = map[connection.to].x - map[connection.to].r
-        const y2 = map[connection.to].y
-        draw("line", {
-            x1,
-            y1,
-            x2,
-            y2,
-            stroke: "red",
-            "stroke-width": 2
-        })
+        const x1 = map[connection["<"]].x + map[connection["<"]].r
+        const y1 = map[connection["<"]].y
+        const x2 = map[connection[">"]].x - map[connection[">"]].r
+        const y2 = map[connection[">"]].y
+        draw("path", { d: `M ${x1} ${y1} L ${x2} ${y2}`, fill: "none", stroke: "red", "stroke-width": 2 })
     })
 }
 
-present(network)
+const visualize = () => {
+    const topology = network.encode()
+    present(topology)
+
+    const test = XOR.map(d => network.calculate(d.input)[0])
+    console.log(test)
+
+    network.mutate()
+    setTimeout(() => {
+        // while (svg.lastChild) svg.removeChild(svg.lastChild)
+        svg.innerHTML = ""
+        visualize()
+    }, 1000)
+}
+
+visualize()
