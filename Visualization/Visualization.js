a27c8cf1aa73fb782bd9b74be1da32339b988112
MIMIZA <dev@mimiza.com>
2022-07-12 21:59:04 +0700

    finished prototyping Visualization

--- Visualization/Visualization.js
+++ Visualization/Visualization.js
@@ -23,24 +23,30 @@ const present = (data = {}) => {
     const w = svg.width.baseVal.value
     const h = svg.height.baseVal.value
     const lw = w / data.l.length // Layer width.
+    const r = lw * 0.2 // Neuron radius.
     const map = {}
     data.l.forEach((layer, lindex) => {
         const neurons = layer.n || layer
         neurons.forEach((neuron, nindex) => {
             const rh = h / neurons.length // Row height.
-            const r = lw * 0.2 // Neuron radius.
             const x = lw * lindex + lw / 2
             const y = rh * nindex + rh / 2
-            draw("circle", { cx: x, cy: y, r, fill: "none", stroke: "red", "stroke-width": 2 })
+            draw("circle", { cx: x, cy: y, r, fill: "none", stroke: "black", "stroke-width": 4 })
             map[neuron] = { x, y, r }
         })
     })
     data.c.forEach(connection => {
+        if (!connection || !data.n[connection["<"]].s || !data.n[connection[">"]].s) return
+        const curve = r * 3
         const x1 = map[connection["<"]].x + map[connection["<"]].r
         const y1 = map[connection["<"]].y
         const x2 = map[connection[">"]].x - map[connection[">"]].r
         const y2 = map[connection[">"]].y
-        draw("path", { d: `M ${x1} ${y1} L ${x2} ${y2}`, fill: "none", stroke: "red", "stroke-width": 2 })
+        const cx1 = x1 + curve
+        const cy1 = x1 >= x2 && y1 === y2 ? y1 - curve : y1
+        const cx2 = x2 - curve
+        const cy2 = x1 >= x2 && y1 === y2 ? y2 - curve : y2
+        draw("path", { d: `M ${x1} ${y1} C ${cx1} ${cy1} ${cx2} ${cy2} ${x2} ${y2}`, fill: "none", stroke: x1 < x2 ? "blue" : "green", "stroke-width": 2 })
     })
 }
 
@@ -56,7 +62,7 @@ const visualize = () => {
         // while (svg.lastChild) svg.removeChild(svg.lastChild)
         svg.innerHTML = ""
         visualize()
-    }, 1000)
+    }, 10)
 }
 
 visualize()
